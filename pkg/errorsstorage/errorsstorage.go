ef67005a2bcb1ad9daf74ec7624b87651ba0ad4b
rnikbond <rnikbond@gmail.com>
2022-04-29 17:35:32 +0300

    added package with storage errors

---
+++ pkg/errorsstorage/errorsstorage.go
@@ -0,0 +1,26 @@
+package errorsstorage
+
+import (
+	"errors"
+	"net/http"
+)
+
+var (
+	ErrorNotFound       = errors.New("metric not found")
+	ErrorUnknownType    = errors.New("metric has unknown type")
+	ErrorIncorrectValue = errors.New("metric has incorrect value")
+)
+
+// ConvertToHTTP Преобразование ошибки Storage в HTTP код
+func ConvertToHTTP(err error) int {
+	switch err {
+	case ErrorNotFound:
+		return http.StatusNotFound
+	case ErrorUnknownType:
+		return http.StatusNotImplemented
+	case ErrorIncorrectValue:
+		return http.StatusBadRequest
+	}
+
+	return http.StatusOK
+}
