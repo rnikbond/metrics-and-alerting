ca228db23dd18b2a312cc34ce9cdb5552679a3d9
rnikbond <rnikbond@gmail.com>
2022-05-27 19:58:21 +0300

    changed time interval for ping storage

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -170,7 +170,7 @@ func (dbStore DataBaseStorage) Ping() bool {
 		return false
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
 	err := dbStore.Driver.PingContext(ctx)
